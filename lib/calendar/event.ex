dce143cff083de2c0fade1cb72960e160d19990c
dinhtungtp <tung6192@gmail.com>
2017-08-09 16:50:21 +0700

    add event functionality

---
+++ lib/calendar/event.ex
@@ -0,0 +1,55 @@
+defmodule GoogleCalendar.Event do
+  import GoogleCalendar.Response
+  import OAuth2.Client
+
+  @base_url "#{Application.get_env(:oauth2_example, :base_url)}/calendars"
+  @content_type Application.get_env(:oauth2_example, :content_type)
+
+  def list(client, %{calendar_id: calendar_id}) do
+    path = "#{@base_url}/#{calendar_id}/events"
+
+    client
+    |> get!(path)
+    |> show_resp("Get event list")
+  end
+
+  def get(client, %{calendar_id: calendar_id, id: id}) do
+    path = "#{@base_url}/#{calendar_id}/events/#{id}"
+
+    client
+    |> get!(path)
+    |> show_resp("Get event information")
+  end
+
+  def insert(client, %{calendar_id: calendar_id} = event) do
+    path = "#{@base_url}/#{calendar_id}/events"
+
+    client
+    |> post!(path, event, [@content_type])
+    |> show_resp("Insert event")
+  end
+
+  def update(client, %{calendar_id: calendar_id, id: id} = event) do
+    path = "#{@base_url}/#{calendar_id}/events/#{id}"
+
+    client
+    |> put!(path, event, [@content_type])
+    |> show_resp("Update event")
+  end
+
+  def patch(client, %{calendar_id: calendar_id, id: id} = event) do
+    path = "#{@base_url}/#{calendar_id}/events/#{id}"
+
+    client
+    |> patch!(path, event, [@content_type])
+    |> show_resp("Update event")
+  end
+
+  def delete(client, %{calendar_id: calendar_id, id: id}) do
+    path = "#{@base_url}/#{calendar_id}/events/#{id}"
+
+    client
+    |> delete!(path)
+    |> show_resp("Delete event")
+  end
+end
---
+++ lib/calendar/response.ex
@@ -0,0 +1,13 @@
+defmodule GoogleCalendar.Response do
+
+  def show_resp(resp, action) do
+    case resp.status_code do
+      code when code in 200..209 ->
+        {:ok, "#{action} successfully", resp.body}
+      code when code in 400..599 ->
+        {:error, code, resp.body["error"]["message"]}
+      code ->
+        {:unknown, code, "Unhandled code, #{resp.body}"}
+    end
+  end
+end
